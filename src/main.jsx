34d13525cf7ae59b9d23783454360fc172277dfd
Khant Zaw Phyo <khantzawphyo02@gmail.com>
2025-05-16 14:37:57 +0630

    Revert "Implement Modern UI/UX Design System"
    This reverts commit e1d7af4236953f39682d8b9859a9342a8e08fa4e.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,5 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ThemeProvider } from "./context/theme-provider";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="todo-theme">
-      <App />
-    </ThemeProvider>
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
